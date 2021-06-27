7fbad26cdc81e01e9cf2e4f3b5ea86a1f789cda9
Kennyola <kennyolapower@yahoo.co.uk>
2021-06-27 22:53:28 +0200

    Update largestarray.js

--- largestarray.js
+++ largestarray.js
@@ -2,9 +2,9 @@
 
 
 
-function largestOfFour(mainArray) {
+function largestNo(mainArray) {
    return mainArray.map(function(subArray) {
      return Math.max.apply(null, subArray);
    });
  }
- console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+ console.log(largestNo([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
