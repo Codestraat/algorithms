cc0d19a7db692b15e8af0eeb1f7e81200f7cee15
Kennyola <kennyolapower@yahoo.co.uk>
2021-06-27 22:58:36 +0200

    Update largestarray.js

--- largestarray.js
+++ largestarray.js
@@ -2,9 +2,20 @@
 
 
 
-function largestNo(mainArray) {
-   return mainArray.map(function(subArray) {
-     return Math.max.apply(null, subArray);
-   });
+
+var m =[[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]
+var largest =0;
+var a_fill = [];
+
+for(var x =0; x < m.length; x++){
+  largest = m[x][0];
+ for(var j =0; j < m[x].length; j++){
+
+   if (m[x][j]>largest){
+     largest = m[x][j];
+   }
  }
- console.log(largestNo([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+ a_fill.push(largest);
+}
+
+console.log(a_fill);
