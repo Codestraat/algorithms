e3403ceac88163a92e711c5cbf013b3d8f730eea
Kennyola <kennyolapower@yahoo.co.uk>
2021-07-02 17:52:15 +0200

    Update toupper.js

--- toupper.js
+++ toupper.js
@@ -1,3 +1,13 @@
+//For this solution, we will use the String.prototype.toLowerCase() method, the String.prototype.split() method and the String.prototype.substring()
+// The toLowerCase() method returns the calling string value converted to lowercase
+// The split() method splits a String object into an array of strings by separating the string into substrings
+// The substring() method display characters from the string
+
+// Step 1. Lowercase the string
+// Step 2. Split the string into an array of strings
+// Step 3. Create the FOR 
+//  Return the output
+
 var str="hi all. this is my uppercase. i am trying this out.";
 function toUpperCase(strval){
     
